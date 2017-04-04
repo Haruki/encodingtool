a03c0d882241c6e32d11aaa42a332becb3a8fc45
borsutzha <borsutzky@hansaluftbild.de>
2017-04-04 12:43:34 +0200

    conversion code hinzugefügt.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var jschardet = require("jschardet")
+var iconv = require("iconv-lite")
 // Make sure we got a filename on the command line.
 if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
@@ -7,9 +8,16 @@ if (process.argv.length < 3) {
 // Read the file and print its contents.
 var fs = require('fs')
   , filename = process.argv[2];
-fs.readFile(filename, function(err, data) {
+fs.readFileSync(filename, function(err, data) {
   if (err) throw err;
   console.log('OK: ' + filename);
   //console.log(data);
-  console.log('Detected: ' + jschardet.detect(data).encoding);
+  var detectedEncoding = jschardet.detect(data).encoding;
+  console.log('Detected: ' + detectedEncoding);
 });
+var newFileName = filename + '_iso-8859-1.txt';
+// Convert encoding streaming example
+fs.createReadStream(filename)
+    .pipe(iconv.decodeStream('utf8'))
+    .pipe(iconv.encodeStream('ISO-8859-1'))
+    .pipe(fs.createWriteStream(newFileName));
